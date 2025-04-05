c2a6543e90a137ee4e8a02c9f9289d3a3ab4ec05
AvelinoBatista <alexier1511@gmail.com>
2025-04-04 23:56:45 -0300

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,13 @@ $db_name = "moviestar";
 $db_host = "localhost";
 $db_user = "moviestar";
 $db_pass = "Ale@151180";
-$conn = new PDO("mysql:dbname=". $db_name .";host=". $db_host, $db_user, $db_pass);
-$conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$conn -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+try {
+    $conn = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+} catch (PDOException $e) {
+    echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
+    exit;
+}
 ?>
