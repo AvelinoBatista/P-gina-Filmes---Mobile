6d59c605b6b48e38e1496e0dee2b1fc6ac754ed5
AvelinoBatista <alexier1511@gmail.com>
2025-04-04 23:54:10 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $db_name = "moviestar";
 $db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
+$db_user = "moviestar";
+$db_pass = "Ale@151180";
 $conn = new PDO("mysql:dbname=". $db_name .";host=". $db_host, $db_user, $db_pass);
 $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $conn -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
